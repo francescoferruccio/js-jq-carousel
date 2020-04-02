059b778c51cd1ca68559e1aa762b48340616fd64
Francesco <francesco.ferruccio88@gmail.com>
2020-04-02 14:24:03 +0200

    funzionamento base carousel

--- js/main.js
+++ js/main.js
@@ -2,3 +2,51 @@
 // faccio funzionare il tracciamento dei pallini anche in prev (stamattina lo abbiam fatto solo in next);
 // creo anche la possibilità di poter navigare cliccando anche sui pallini (quindi se clicco sul pallino 3, l’img vista è la 3 e il pallino 3 rimane rosso);
 // cerco di fare un refactoring del mio codice per renderlo il più chiaro, lineare, mantenibile e secco possibile (dobbiamo iniziare a farlo, e quindi prima del bonus).
+
+$(document).ready(function() {
+
+  // al click sull'icona next eseguo la funzione nextImg
+  $(".next i").click(nextImg);
+  // al click sull'icona prev eseguo la funzione prevImg
+  $(".prev i").click(prevImg);
+
+  // creo la funzione nextImg che scorre le immagini del carousel
+  function nextImg() {
+    // seleziono l'immagine con classe active, quindi quella attualmente visibile
+    var imgActive = $(".images img.active");
+    // tolgo la classe "active" all'elemento selezionato
+    imgActive.removeClass("active");
+
+    // controllo se l'immagine selezionata è l'ultima
+    if(imgActive.hasClass("last")) {
+      // se è l'ultima aggiungo la classe active alla prima img
+      $(".images img.first").addClass("active");
+    } else {
+      // altrimenti assegno la classe active alla prossima img
+      imgActive.next().addClass("active");
+    }
+  }
+
+  // applico la stessa logica ma invertita per la funzione prevImg
+  function prevImg() {
+    // seleziono l'immagine con classe active
+    var imgActive = $(".images img.active");
+    // tolgo la classe "active" all'elemento selezionato
+    imgActive.removeClass("active");
+
+    // controllo se l'immagine selezionata è la prima
+    if(imgActive.hasClass("first")) {
+      // se è la prima aggiungo la classe active all'ultima img
+      $(".images img.last").addClass("active");
+    } else {
+      // altrimenti assegno la classe active alla precedent img
+      imgActive.prev().addClass("active");
+    }
+  }
+
+
+
+
+
+
+});
