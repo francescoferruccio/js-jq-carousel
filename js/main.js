9be110930d23b752e05c483eac3ae4dbd05b7067
Francesco <francesco.ferruccio88@gmail.com>
2020-04-02 14:00:34 +0200

    struttura base html

---
+++ js/main.js
@@ -0,0 +1,4 @@
+// ricreo lo slider come fatto questa mattina (o da zero, o partendo dall’html [ref sempre mio repo su github]);
+// faccio funzionare il tracciamento dei pallini anche in prev (stamattina lo abbiam fatto solo in next);
+// creo anche la possibilità di poter navigare cliccando anche sui pallini (quindi se clicco sul pallino 3, l’img vista è la 3 e il pallino 3 rimane rosso);
+// cerco di fare un refactoring del mio codice per renderlo il più chiaro, lineare, mantenibile e secco possibile (dobbiamo iniziare a farlo, e quindi prima del bonus).
